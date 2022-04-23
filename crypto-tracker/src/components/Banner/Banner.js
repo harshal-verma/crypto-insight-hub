5432cd8917f2c7b98c2c6165ad407f9b72556ba1
Harshal Verma <harshalvermaaaaa@gmail.com>
2022-04-23 14:23:02 +0530

    minor change

--- crypto-tracker/src/components/Banner/Banner.js
+++ crypto-tracker/src/components/Banner/Banner.js
@@ -41,7 +41,7 @@ function Banner() {
                             fontFamily: "Montserrat",
                         }}
                     >
-                        Crypto Tracker
+                        Krypto Tracker
                     </Typography>
                     <Typography
                         variant="subtitle2"
--- crypto-tracker/src/components/Header.js
+++ crypto-tracker/src/components/Header.js
@@ -52,7 +52,7 @@ function Header() {
                             variant="h6"
                             className={classes.title}
                         >
-                            Crypto Tracker
+                            Krypto Tracker
                         </Typography>
                         {/* <Button color="inherit">Login</Button> */}
                         <Select
